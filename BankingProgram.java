a89280dc1c26aa41b382d38959b072ed684aa770
VaggelisVer <115818122+VaggelisVerPy@users.noreply.github.com>
2025-12-12 13:34:02 +0200

    Add files via upload

---
+++ BankingProgram.java
@@ -0,0 +1,107 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class BankingProgram {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // JAVA BANKING PROGRAM FOR BEGINNERS
+
+        // 1. DECLARE VARIABLES
+
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
+
+        // DISPLAY MENU
+
+        while(isRunning) {
+            System.out.println("****************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("****************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("****************");
+
+            // GET AND PROCESS USERS CHOICE
+
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> balance -= withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("Invalid choice");
+            }
+        }
+
+        System.out.println("****************");
+        System.out.println("Thank you! Have a nice day!");
+        System.out.println("****************");
+
+        // showBalance()
+
+        // deposit()
+
+        // withdraw()
+
+        // EXIT MESSAGE
+
+        scanner.close();
+    }
+
+    static void showBalance(double balance) {
+        System.out.println("****************");
+        System.out.printf("€%.2f\n", balance);
+    }
+
+    static double deposit() {
+
+        double amount;
+
+        System.out.print("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+
+        if(amount < 0) {
+            System.out.println("****************");
+            System.out.println("Amount can't be negative");
+            return 0;
+        }
+        else {
+            System.out.println("****************");
+            System.out.println("Amount deposited successfully");
+            return amount;
+        }
+    }
+
+    static double withdraw(double balance) {
+
+        double amount;
+
+        System.out.print("Enter amount to be withdrawn: ");
+        amount = scanner.nextDouble();
+
+        if(amount > balance) {
+            System.out.println("****************");
+            System.out.println("INSUFFICIENT FUNDS");
+            return 0;
+        }
+        else if(amount < 0) {
+            System.out.println("****************");
+            System.out.println("Amount can't be negative");
+            return 0;
+        }
+        else {
+            System.out.println("****************");
+            System.out.println("Amount withdrawn successfully");
+            return amount;
+        }
+    }
+}
